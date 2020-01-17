29eb6da4b241163390fa06c86ef537a8ebb3a69a
benjaminxu251 <39343316+benjaminxu251@users.noreply.github.com>
2020-01-17 17:31:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ async def remove(ctx):
 
         sent1 = await ctx.send(embed=messages.remove_1)
         def check(m):
-            return ((m.content == '$remove') or not(m.startsWith('$')))
+            return ((m.content == '$remove') or (m.content == 'c') or not(m.startsWith('$')))
         start = await client.wait_for('message', timeout=20.0,check=check)
         await asyncio.sleep(0.5)
         await sent1.delete()
@@ -212,8 +212,6 @@ async def remove(ctx):
         for i in range (0, 3):
             if (len(global_teams[author][i] == 0)):
                 global_teams[author][i] = [None]
-
-
 
     except asyncio.TimeoutError:
         await sent.delete()
