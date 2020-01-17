27f62c605ff2fde78f9ea5eee1b5eb41b142ce7d
benjaminxu251 <39343316+benjaminxu251@users.noreply.github.com>
2020-01-17 16:45:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def add(ctx):
             raise exceptions.Cancel()
 
         sent2 = await ctx.send(embed=messages.add_2)
-        toaddname = await client.wait_for('message', timeout=30.0, check=check)
+        toaddname = await client.wait_for('message', timeout=30.0)
         await asyncio.sleep(0.5)
         await sent2.delete()
         if toaddname.content == 'c':
