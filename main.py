1cf89d901f61cf7c712ceaf5d91f682e8f321692
merrickt <39343323+merrickt@users.noreply.github.com>
2020-01-18 15:01:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
 import asyncio
-import resources/exceptions
-import resources/uncap
-import resources/messages
+from resources import exceptions
+from resources import uncap
+from resources import messages
 import calculator
 import json
 #Add mode modules as we add more
