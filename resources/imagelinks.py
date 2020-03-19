8b20ea34aff02aa46cf2dfc7bc76e37a270c2e85
merrickt <39343323+merrickt@users.noreply.github.com>
2020-03-19 19:14:56 -0400

    Update imagelinks.py

--- resources/imagelinks.py
+++ resources/imagelinks.py
@@ -1,13 +1,33 @@
 import discord
 
-#discord.Color.from_rgb(141, 229, 95) - light green - for wind
+wind = discord.Color.from_rgb(141, 229, 95)
+fire = discord.Color.from_rgb(159, 95, 35)
+water = discord.Color.from_rgb(76, 157, 225)
+earth = discord.Color.from_rgb(180, 119, 63)
+light = discord.Color.from_rgb(254, 236, 109)
+dark = discord.Color.from_rgb(139, 82, 212)
+grey = discord.Color.from_rgb(169, 167, 165)
 
 images = {
+  'abby' : [
+  (fire,'https://gbf.wiki/images/5/50/Npc_zoom_3030011000_01.png'),
+  (fire,'https://gbf.wiki/images/7/79/Npc_zoom_3030011000_02.png')
+  ],
+  'agielba' : [
+  (fire,'https://gbf.wiki/images/6/68/Npc_zoom_3040017000_01.png'),
+  (fire,'https://gbf.wiki/images/3/3a/Npc_zoom_3040017000_02.png'),
+  ],
+  'aglovale' : [
+  (water,'https://gbf.wiki/images/c/c2/Npc_zoom_3040192000_01.png'),
+  (water,'https://gbf.wiki/images/9/98/Npc_zoom_3040192000_02.png'),
+  (water,'https://gbf.wiki/images/f/f1/Aglovale%2C_Lord_of_Frost_NPC.png'),
+  (grey,'https://gbf.wiki/images/d/d4/House_of_Wales_Dress_Uniform_%28Aglovale%29.png'),
+  ],
   'monika' : [
-  (discord.Color.from_rgb(141, 229, 95),'https://i.imgur.com/3g2iFbF.png'),
-  (discord.Color.blue(),'https://i.imgur.com/qKCZeDu.png'),
-  (discord.Color.from_rgb(141, 229, 95),'https://i.imgur.com/iUkOJnJ.png'),
-  (discord.Color.from_rgb(141, 229, 95),'https://i.imgur.com/mEnmxIV.png'),
-  (discord.Color.from_rgb(141, 229, 95),'https://i.imgur.com/FTaLsbC.png')
+  (wind,'https://i.imgur.com/3g2iFbF.png'),
+  (wind,'https://i.imgur.com/qKCZeDu.png'),
+  (wind,'https://i.imgur.com/iUkOJnJ.png'),
+  (wind,'https://i.imgur.com/mEnmxIV.png'),
+  (wind,'https://i.imgur.com/FTaLsbC.png')
   ]
 }
