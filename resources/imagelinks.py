ff9915422b11e7e84125a2eb9dd0bdb78de48373
merrickt <39343323+merrickt@users.noreply.github.com>
2020-03-19 17:26:04 -0400

    Update imagelinks.py

--- resources/imagelinks.py
+++ resources/imagelinks.py
@@ -1,9 +1,9 @@
 images = {
-  monika : [
-  https://imgur.com/3g2iFbF,
-  https://imgur.com/qKCZeDu,
-  https://imgur.com/iUkOJnJ,
-  https://imgur.com/mEnmxIV,
-  https://imgur.com/FTaLsbC
+  'monika' : [
+  'https://imgur.com/3g2iFbF',
+  'https://imgur.com/qKCZeDu',
+  'https://imgur.com/iUkOJnJ',
+  'https://imgur.com/mEnmxIV',
+  'https://imgur.com/FTaLsbC'
   ]
 }
