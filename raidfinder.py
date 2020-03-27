cd28c5fc7e7229922c2767253bd0c991854fb4e1
merrickt <39343323+merrickt@users.noreply.github.com>
2020-03-26 23:59:02 -0400

    Update raidfinder.py

--- raidfinder.py
+++ raidfinder.py
@@ -12,7 +12,8 @@ async def send_msg(ctx, msg, bot_client):
     try:
         # if a user clicks on a react then the image changes
         def react_check(reaction, user):
-            print(str(user))
+            print((reaction.message.id == sent.id and str(reaction.emoji) == '🇨'
+                    and (not user == 'Sierokarte Bot#5823'))
             return (reaction.message.id == sent.id and str(reaction.emoji) == '🇨'
                     and (not user == 'Sierokarte Bot#5823'))
 
